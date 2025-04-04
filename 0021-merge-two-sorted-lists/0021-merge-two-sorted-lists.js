ea2bd7301f06e86ad037b1fd5848bc5cc012b309
김상주 <rlatkdwn3379@gmail.com>
2025-04-04 09:05:02 +0900

    Time: 2 ms (17.68%), Space: 57.9 MB (27.34%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -12,17 +12,18 @@
  */
 var mergeTwoLists = function(list1, list2) {
     let dummy = new ListNode();
-    let current = dummy
+    let current = dummy;
+
     while(list1 !== null && list2 !== null){
         if(list1.val < list2.val){
-            current.next = list1
-            list1 = list1.next
+            current.next = list1;
+            list1 = list1.next;
         }else{
-            current.next = list2
-            list2 = list2.next
+            current.next = list2;
+            list2 = list2.next;
         }
-        current = current.next
+        current = current.next;
     }
-    current.next = list1 || list2
-    return dummy.next
+    current.next = list1 || list2;
+    return dummy.next;
 };
