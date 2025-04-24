37245baddc83fd76a4665d7ba26455a859cc5bec
김상주 <rlatkdwn3379@gmail.com>
2025-04-24 23:15:52 +0900

    Time: 0 ms (100%), Space: 53.8 MB (24.73%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,16 @@
+function climbStairs(n) {
+  if (n === 1) return 1;
+  if (n === 2) return 2;
+
+  let first = 1;
+  let second = 2;
+
+  for (let i = 3; i <= n; i++) {
+    let third = first + second;
+    first = second;
+    second = third;
+  }
+
+  return second;
+}
+
